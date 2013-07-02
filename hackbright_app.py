a9ef304942356974908b4be5e4bbdd18472edc19
Hackbright Student <user@mini-006.(none)>
2013-07-02 16:39:36 -0700

    sql_lesson first day

--- hackbright_app.py
+++ hackbright_app.py
@@ -16,12 +16,40 @@ def connect_to_db():
     CONN = sqlite3.connect("hackbright.db")
     DB = CONN.cursor()
 
+def make_new_student(first_name, last_name, github):
+    query = """INSERT into Students values (?,?,?)"""
+    DB.execute(query, (first_name, last_name, github))
+    CONN.commit()
+    print "Successfully added student: %s %s"%(first_name, last_name)
+
+def get_project_by_title(project_title):
+    query = """SELECT title, description FROM Projects WHERE title = ?"""
+    DB.execute(query, (project_title,))
+    row = DB.fetchone()
+    print "The project %s is: %s"%(row[0], row[1])
+
+def make_new_project(title, description, max_grade):
+    query = """INSERT into Projects values (?, ?, ?)"""
+    DB.execute(query, (title, description, max_grade))
+    CONN.commit()
+    print "Successfully added project: %s"%(title)
+
+def get_grade_by_project(title, github):
+    print 'test'
+    query = """SELECT first_name, grade, project_title FROM Students
+            JOIN Grades on (github = student_github) 
+            WHERE project_title = ? and github = ?"""
+    DB.execute(query, (title, github))
+    row = DB.fetchone()
+   
+    print "%s's grade is %d for their project: %s"%(row[0], row[1], row[2])
+
 def main():
     connect_to_db()
     command = None
     while command != "quit":
         input_string = raw_input("HBA Database> ")
-        tokens = input_string.split()
+        tokens = input_string.split('; ')
         command = tokens[0]
         args = tokens[1:]
 
@@ -29,6 +57,14 @@ def main():
             get_student_by_github(*args) 
         elif command == "new_student":
             make_new_student(*args)
+        elif command == "project":
+            get_project_by_title(*args)
+        elif command == "new_project":
+            make_new_project(*args)
+        elif command == "project_grade":
+            get_grade_by_project(*args)
+        else:
+            print "Use an available command: student, new_student, project, new_project, project_grade"
 
     CONN.close()
 
